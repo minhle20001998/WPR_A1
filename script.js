9b8ee5d4a89771a1a3d8e6a295936a79987b1435
minh le <minhle20001998@gmail.com>
2020-10-02 21:16:16 +0700

    re-organise codes, add comments

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 // TODO(you): Write the JavaScript necessary to complete the assignment.
-
+//create an object to store user answers
+let userAnswers = {};
+const intro = document.querySelector('#introduction');
+const author = document.querySelector('#author');
 const button_start = document.querySelector('.blue');
 button_start.addEventListener('click', handleStartButton);
-const userAnswer = {};
 
 function handleStartButton(event) {
     let body = document.body;
-    let intro = document.querySelector('#introduction');
-    let author = document.querySelector('#author');
     intro.style.display = 'none';
     author.style.display = 'none';
+    //if start button pressed, call API
     fetchData();
 }
 
-function handleAnsClick(e) {
-    userAnswer[e.target.name] = e.target.value;
-    console.log('-----', userAnswer)
-}
-
+//calling API
 async function fetchData() {
     const url = "https://wpr-quiz-api.herokuapp.com/attempts";
     const myResponse = await fetch(url, {
@@ -26,23 +23,70 @@ async function fetchData() {
             "Content-Type": "application/json",
         },
     });
-    //begin
     const myJson = await myResponse.json();
     const quizBody = document.querySelector(".quiz-body");
+    const attempQuiz = document.querySelector("#attempt-quiz")
     const button_submit = document.createElement('button');
     const button_div = document.createElement('div');
+
 
     button_submit.textContent = "Submit your answers >"
     button_submit.classList.add("green");
     button_div.classList.add("box");
     button_div.appendChild(button_submit);
-    button_submit.addEventListener('click', handleSubmit)
 
-    //submit button
+
+    let qName = 0;
+    let count = 1;
+    //generate questions and answers from JSON 
+    myJson.questions.forEach(e => {
+        const titles = document.createElement('p');
+        const questions = document.createElement('p');
+        titles.classList.add("title");
+        titles.textContent = `Question ${count} of ${myJson.questions.length}`;
+
+        questions.classList.add("questions");
+        questions.textContent = `${e.text}`;
+
+        quizBody.appendChild(titles);
+        quizBody.appendChild(questions);
+        for (let i = 0; i < e.answers.length; i++) {
+            userAnswers[e._id] = null;
+            const label = document.createElement('label');
+            const input = document.createElement('input');
+            const div = document.createElement('div');
+
+            input.type = "radio";
+            input.id = `Q${qName}`;
+            input.name = `${e._id}`;
+            input.value = `${i}`;
+            input.addEventListener('click', handleAnsClick)
+
+            //if radio input checked -> store the index of radio input
+            function handleAnsClick(e) {
+                userAnswers[e.target.name] = e.target.value;
+                // console.log('-----', userAnswer)
+            }
+
+            label.htmlFor = `Q${qName}`;
+            label.textContent = e.answers[i];
+            div.classList.add('background');
+
+            div.appendChild(input);
+            div.appendChild(label);
+            quizBody.appendChild(div);
+            qName++;
+        }
+        count += 1;
+    });
+    //append submit button
+    quizBody.appendChild(button_div);
+
+    //handle submit button
+    button_submit.addEventListener('click', handleSubmit)
     function handleSubmit(event) {
-        event.currentTarget.style.display = 'none';
+        button_div.style.display = 'none';
         const review_quiz = document.querySelector('#review-quiz');
-        console.log(review_quiz);
         const disable = document.querySelectorAll('input');
         const correctAns = myJson.correctAnswers;
         const result_view = document.createElement('div');
@@ -53,14 +97,13 @@ async function fetchData() {
         const result_comment = document.createElement('p')
         let scores = 0;
         let result = '';
-
         //disable input
         disable.forEach(e => {
             e.disabled = true;
         });
-        for (const key in userAnswer) {
+        for (const key in userAnswers) {
             const totalInputDom = document.querySelectorAll(`input[name='${key}']`);
-            const userInputDom = totalInputDom[userAnswer[key]];
+            const userInputDom = totalInputDom[userAnswers[key]];
             const correctInputDOM = totalInputDom[correctAns[key]];
             const statusCorrect = document.createElement('div');
             const statusWrong = document.createElement('div');
@@ -68,11 +111,10 @@ async function fetchData() {
             statusCorrect.classList.add('correct');
             statusWrong.textContent = "Wrong Answer";
             statusWrong.classList.add('wrong');
-
             //if user chose an answer
-            if (userAnswer[key] != null) {
+            if (userAnswers[key] != null) {
                 //if user answer is correct
-                if (userAnswer[key] == correctAns[key]) {
+                if (userAnswers[key] == correctAns[key]) {
                     correctInputDOM.nextElementSibling.style.backgroundColor = '#d4edda'
                     correctInputDOM.nextElementSibling.appendChild(statusCorrect);
                     scores++;
@@ -91,7 +133,7 @@ async function fetchData() {
                 correctInputDOM.nextElementSibling.appendChild(statusCorrect);
             }
         }
-        //write comment
+        //write score comment
         if (scores < 5) {
             result = 'Practice more to improve it :D';
         }
@@ -107,18 +149,25 @@ async function fetchData() {
 
         //setup result box
         result_view.classList.add("box");
-        titles.classList.add('title');
+        titles.classList.add('result_title');
         titles.textContent = "Result:"
         result_score.classList.add('score');
         result_score.textContent = `${scores}/10`;
         result_percent.classList.add('questions');
         result_percent.textContent = `${scores * 10}%`;
         button_again.textContent = "Try Again";
-        button_again.addEventListener('click', function () {
-            location.reload();
-        })
+        button_again.addEventListener('click', handleTryAgain);
         button_again.classList.add('blue');
         result_comment.textContent = result;
+
+        //handle try again button
+        function handleTryAgain(e) {
+            quizBody.innerHTML = "";
+            review_quiz.innerHTML = "";
+            intro.style.display = "block";
+            userAnswers = {};
+        }
+
         //append result box
         result_view.appendChild(titles);
         result_view.appendChild(result_score);
@@ -126,50 +175,7 @@ async function fetchData() {
         result_view.appendChild(result_comment);
         result_view.appendChild(button_again);
         review_quiz.appendChild(result_view);
-
-
-
-
-
     }
-    let qName = 0;
-    let count = 1;
-    myJson.questions.forEach(e => {
-        const titles = document.createElement('p');
-        const questions = document.createElement('p');
-        titles.classList.add("title");
-        titles.textContent = `Question ${count} of ${myJson.questions.length}`;
-
-        questions.classList.add("questions");
-        questions.textContent = `${e.text}`;
-
-        quizBody.appendChild(titles);
-        quizBody.appendChild(questions);
-        for (let i = 0; i < e.answers.length; i++) {
-            userAnswer[e._id] = null;
-            const label = document.createElement('label');
-            const input = document.createElement('input');
-            const div = document.createElement('div');
-
-            input.type = "radio";
-            input.id = `Q${qName}`;
-            input.name = `${e._id}`;
-            input.value = `${i}`;
-            input.addEventListener('click', handleAnsClick)
-
-            label.htmlFor = `Q${qName}`;
-            label.textContent = e.answers[i];
-            div.classList.add('background');
-
-            div.appendChild(input);
-            div.appendChild(label);
-            quizBody.appendChild(div);
-            qName++;
-        }
-        count += 1;
-    });
-    quizBody.appendChild(button_div);
-
 }
 
 
